890a83e5e435722a9ec37300f11c08fb677055f2
csais <c.sais86@gmail.com>
2024-09-26 12:21:01 -0700

    Finished settup and Button component

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+
+const handleClick = () => {
+    console.log("Clicked");
+}
+
+function App() {
+    return (
+        <div>
+            <div>
+                <Button className='mb-5' onClick={handleClick} success rounded outline>
+                    <GoBell/> Click Me!!!
+                    </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    <GoCloudDownload/>
+                    Buy Now!
+                    </Button>
+            </div>
+            <div>
+                <Button warning>
+                    <GoDatabase/>
+                    See Deal!
+                    </Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    Hide Ads!!
+                    </Button>
+            </div>
+            <div>
+                <Button primary rounded>
+                    Something!
+                    </Button>
+            </div>
+        </div>
+    );
+};
+
+export default App;
